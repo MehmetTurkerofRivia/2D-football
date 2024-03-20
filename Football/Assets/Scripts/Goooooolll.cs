1fdb867145708a2b0e6db90f20c1775c510acac7
MehmetTurkerofRivia <mertmemo69@gmail.com>
2024-03-20 13:04:57 +0300

    a litle bit

--- Football/Assets/Scripts/Goooooolll.cs
+++ Football/Assets/Scripts/Goooooolll.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Goooooolll : MonoBehaviour
 {
     private AudioSource audioSource;
-    public AudioClip collisionSound; // Çarpýþma sesi
+    public AudioClip collisionSound;
 
     void Start()
     {
-        // AudioSource bileþenini almak için GetComponent kullanýlýr
         audioSource = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Ball"))
         {
+            Debug.Log("a");
+    
             audioSource.PlayOneShot(collisionSound);
         }
     }
